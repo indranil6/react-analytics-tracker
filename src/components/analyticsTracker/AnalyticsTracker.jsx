beb6207ce3978ea953d4442b61aa53347538f13d
Indranil Kundu <indranil.kundu@clirnet.com>
2024-07-04 22:14:54 +0530

    customs added

--- src/components/analyticsTracker/AnalyticsTracker.jsx
+++ src/components/analyticsTracker/AnalyticsTracker.jsx
@@ -3,20 +3,107 @@ import React, { Component } from "react";
 let intervalid;
 let visibilityChange;
 const REPORT_HEARTBEAT_INTERVAL = 4000;
-let eventCollections = [];
 
 export class AnalyticsTracker extends Component {
+  constructor(props) {
+    super(props);
+    this.heartBeatInterval =
+      props.heartBeatInterval || REPORT_HEARTBEAT_INTERVAL;
+
+    this.sessionId =
+      window.sessionStorage.getItem("sessionId") || this.generateSessionId();
+    window.sessionStorage.setItem("rat:sessionId", this.sessionId);
+    console.log("Session id:", this.sessionId);
+    this.eventCollections = [];
+  }
+  generateSessionId() {
+    return (
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15)
+    );
+  }
+  getCookieValue(name) {
+    // Create a regular expression to find the cookie with the specified name
+    const nameEQ = name + "=";
+    const cookies = document.cookie.split(";");
+
+    // Iterate through the cookies
+    for (let i = 0; i < cookies.length; i++) {
+      let cookie = cookies[i].trim();
+
+      // Check if the cookie's name matches the specified name
+      if (cookie.indexOf(nameEQ) === 0) {
+        // Return the value of the cookie
+        return cookie.substring(nameEQ.length, cookie.length);
+      }
+    }
+
+    // Return null if the cookie was not found
+    return null;
+  }
   async report(callback = () => {}) {
-    let response = await fetch(this.props.reportingEndpoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(eventCollections),
-    });
-    if (response.ok) {
-      eventCollections.length = 0;
-      callback();
+    if (this.eventCollections.length > 0) {
+      try {
+        console.log("Sending analytics data to server...");
+        console.log("Event collections:", this.eventCollections);
+
+        let payload = {
+          referrer:
+            document.referrer || window.location.ancestorOrigins["0"] || "",
+          url: window.location.href,
+          pathname: window.location.pathname,
+          hostname: window.location.hostname,
+          title: document.title,
+          screen: window.screen.width + "x" + window.screen.height,
+          language: navigator.language,
+          utmSource: this.getCookieValue("utm_source"),
+          utmMedium: this.getCookieValue("utm_medium"),
+          utmCampaign: this.getCookieValue("utm_campaign"),
+          utmTerm: this.getCookieValue("utm_term"),
+          utmContent: this.getCookieValue("utm_content"),
+          sessionId: this.sessionId,
+          network: this.getNetworkConnectionInfo(),
+          events: this.eventCollections,
+        };
+        if (this.props.customPayload) {
+          Object.assign(payload, this.props.customPayload);
+        }
+
+        console.log("Payload:", payload);
+        if (this.props.reportingEndpoint) {
+          let response = await fetch(this.props.reportingEndpoint, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(this.eventCollections),
+          });
+          if (response.ok) {
+            console.log("Successfully sent analytics data to server!");
+            this.eventCollections.length = 0;
+            callback();
+          } else {
+            console.log(
+              "Failed to send analytics data to server:",
+              response.status
+            );
+            this.eventCollections.length = 0;
+            callback();
+          }
+        } else if (
+          this.props.onReport &&
+          typeof this.props.onReport === "function"
+        ) {
+          this.props.onReport(payload);
+
+          this.eventCollections.length = 0;
+          callback();
+        }
+      } catch (error) {
+        console.log("Error sending analytics data to server:", error);
+        this.eventCollections.length = 0;
+        callback();
+      }
     }
   }
 
@@ -49,68 +136,73 @@ export class AnalyticsTracker extends Component {
 
   trackEvent(eventName, data) {
     console.log(`Event: ${eventName}`, data);
+
+    let newEventRow = {
+      data: data.eventData,
+      event: eventName,
+      element: data.elementName,
+      component: data.componentName,
+      timestamp: Date.now(),
+    };
+    if (this.props.customProperties) {
+      Object.assign(newEventRow, this.props.customProperties);
+    }
+    this.eventCollections.push(newEventRow);
     if (eventName === "view") {
       console.log("Tracking view event...");
-      if (eventCollections.length > 4) {
+
+      if (this.eventCollections.length > 4) {
         this.report(() => {
-          eventCollections.push({
-            data: data.eventData,
-            event: eventName,
-            element: data.elementName,
-            component: data.componentName,
-          });
-        });
-      } else {
-        eventCollections.push({
-          data: data.eventData,
-          event: eventName,
-          element: data.elementName,
-          component: data.componentName,
+          console.log("Reported!");
         });
       }
     } else if (eventName === "click") {
       console.log("Tracking click event...");
-      let postData = [];
-      postData.push({
-        data: data.eventData ? JSON.stringify(data.eventData) : "",
-        event: eventName,
-        element: data.elementName,
-        component: data.componentName,
-      });
-      eventCollections = eventCollections.concat(postData);
+
       this.report();
     }
   }
 
   handleClick = (event) => {
     console.log("Click event caught!");
-    const target = event.target.closest("[data-component]");
+    const target = event.target.closest("[data-component]") || null;
 
-    if (target) {
-      const elementName =
-        event.target.getAttribute("data-element") || event.target.tagName;
+    const elementName =
+      event.target.getAttribute("data-element") || event.target.tagName;
 
-      const data = event.target.getAttribute("data-element-data");
-      console.log("Tracking click event...");
-      console.log("Component:", elementName);
-      console.log("Data:", data);
-      this.trackEvent("click", {
-        eventData: data,
-        elementName,
-        componentName: target.getAttribute("data-component"),
-      });
-    }
+    const data = event.target.getAttribute("data-element-data");
+    console.log("Tracking click event...");
+    console.log("Component:", elementName);
+    console.log("Data:", data);
+    let clickCoords =
+      event.nativeEvent.offsetX + "x" + event.nativeEvent.offsetY;
+    console.log("Click coordinates:", clickCoords);
+    let clickData = JSON.stringify({ clickCoords, data });
+    this.trackEvent("click", {
+      eventData: clickData,
+      elementName,
+      componentName: target ? target.getAttribute("data-component") : "",
+    });
   };
 
   handleIntersect = (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         const componentName = entry.target.getAttribute("data-component");
-        const data = entry.target.getAttribute("data-element-data");
+        const data = entry.target.getAttribute("data-component-data");
         console.log("Tracking view event...");
         console.log("Component:", componentName);
+        let scrollPercentage = entry.intersectionRatio * 100;
+        console.log("Scroll percentage:", scrollPercentage);
+        let scrollPosition = window.pageYOffset;
+        console.log("Scroll position:", scrollPosition);
+        let scrollData = JSON.stringify({
+          scrollPercentage,
+          scrollPosition,
+          data: data || "",
+        });
         this.trackEvent("view", {
-          eventData: data,
+          eventData: scrollData,
           componentName,
           elementName: "",
         });
@@ -173,7 +265,7 @@ export class AnalyticsTracker extends Component {
     document.addEventListener(visibilityChange, this.handleVisibilityChange);
     intervalid = setInterval(() => {
       this.report();
-    }, REPORT_HEARTBEAT_INTERVAL);
+    }, this.heartBeatInterval);
   }
 
   componentWillUnmount() {
